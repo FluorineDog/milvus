3b6fb70c3adbb55660d3cee0a6e1a94d24b289fb
fluorinedog <fluorinedog@gmail.com>
2020-09-06 17:00:57 +0800

    concurrent_vector

--- core/src/dog_segment/ConcurrentVector.h
+++ core/src/dog_segment/ConcurrentVector.h
@@ -1,27 +1,113 @@
 #pragma once
 #include <tbb/concurrent_vector.h>
 
+#include <cassert>
 #include <vector>
 namespace milvus::dog_segment {
 
-
 // we don't use std::array because capacity of concurrent_vector wastes too much memory
-template<typename Type, int Size>
-class FixedVector: public std::vector<Type> {
+template <typename Type>
+class FixedVector : public std::vector<Type> {
  public:
-    FixedVector(): std::vector<Type>(Size) {}
+    // This is a stupid workaround for tbb API to avoid memory copy
+    explicit FixedVector(int64_t size) : placeholder_size_(size) {
+    }
+    FixedVector(const FixedVector<Type>& placeholder_vec)
+        : std::vector<Type>(placeholder_vec.placeholder_size_), is_placeholder_(false) {
+        assert(placeholder_vec.is_placeholder_);
+    }
+    FixedVector(FixedVector<Type>&&) = delete;
+
+    FixedVector&
+    operator=(FixedVector<Type>&&) = delete;
+
+    FixedVector&
+    operator=(const FixedVector<Type>&) = delete;
+
+    bool is_placeholder() {
+        return is_placeholder_;
+    }
+ private:
+    bool is_placeholder_ = true;
+    int placeholder_size_ = 0;
 };
 
-template<typename Type, int Dim>
+template <typename Type, ssize_t ElementsPerChunk = 32 * 1024>
 class ConcurrentVector {
  public:
     // constants
-    static constexpr ssize_t ElementsPerChunk = 32 * 1024; // TODO: Configuarable
-    static constexpr ssize_t DataPerChunk = Dim * ElementsPerChunk;
-    using Chunk = FixedVector<Type, DataPerChunk>;
- public:
+    using Chunk = FixedVector<Type>;
 
+ public:
+    ConcurrentVector(ssize_t dim) : Dim(dim), SizePerChunk(dim * ElementsPerChunk) {
+    }
+    void
+    grow_to_at_least(int64_t element_count) {
+        auto chunk_count = 1 + (element_count - 1) / ElementsPerChunk;
+        chunks_.grow_to_at_least(chunk_count, FixedVector<Type>(SizePerChunk));
+    }
+
+    void
+    set_data(ssize_t element_offset, const Type* source, ssize_t element_count) {
+        auto chunk_id = element_offset / ElementsPerChunk;
+        auto chunk_offset = element_offset % ElementsPerChunk;
+        ssize_t source_offset = 0;
+        // first partition:
+        if (chunk_offset + element_count <= ElementsPerChunk) {
+            // only first
+            fill_chunk(chunk_id, chunk_offset, element_count, source, source_offset);
+            return;
+        }
+
+        auto first_size = ElementsPerChunk - chunk_offset;
+        fill_chunk(chunk_id, chunk_offset, first_size, source, source_offset);
+
+        source_offset += ElementsPerChunk - chunk_offset;
+        element_count -= first_size;
+        ++chunk_id;
+
+        // the middle
+        while (element_count >= ElementsPerChunk) {
+            fill_chunk(chunk_id, 0, ElementsPerChunk, source, source_offset);
+            source_offset += ElementsPerChunk;
+            element_count -= ElementsPerChunk;
+            ++chunk_id;
+        }
+
+        // the final
+        if(element_count > 0) {
+            fill_chunk(chunk_id, 0, element_count, source, source_offset);
+        }
+    }
+
+    const Chunk&
+    get_chunk(ssize_t chunk_index) {
+        return chunks_[chunk_index];
+    }
+
+    // just for fun, don't use it directly
+    const Type* get_element(ssize_t element_index) {
+        auto chunk_id = element_index / ElementsPerChunk;
+        auto chunk_offset = element_index % ElementsPerChunk;
+        return get_chunk(chunk_id).data() + chunk_offset * Dim;
+    }
+
+    ssize_t chunk_size() {
+        return chunks_.size();
+    }
+
+ private:
+    void
+    fill_chunk(ssize_t chunk_id, ssize_t chunk_offset, ssize_t element_count, const Type* source, ssize_t source_offset) {
+        Chunk& chunk = chunks_[chunk_id];
+        assert(!chunk.is_placeholder());
+        std::copy_n(source + source_offset * Dim, element_count * Dim, chunk.data() + chunk_offset * Dim);
+    }
+
+    const ssize_t Dim;
+    const ssize_t SizePerChunk;
  private:
     tbb::concurrent_vector<Chunk> chunks_;
 };
-}
+
+}  // namespace milvus::dog_segment
---
+++ core/unittest/db/test_concurrent_vector.cpp
@@ -0,0 +1,119 @@
+// Copyright (C) 2019-2020 Zilliz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+// with the License. You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied. See the License for the specific language governing permissions and limitations under the License.
+
+#include <fiu-control.h>
+#include <fiu-local.h>
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <string>
+
+#include "db/SnapshotVisitor.h"
+#include "db/Types.h"
+#include "db/snapshot/IterateHandler.h"
+#include "db/snapshot/Resources.h"
+#include "db/utils.h"
+#include "dog_segment/SegmentBase.h"
+#include "knowhere/index/vector_index/helpers/IndexParameter.h"
+#include "segment/SegmentReader.h"
+#include "segment/SegmentWriter.h"
+#include "src/dog_segment/SegmentBase.h"
+#include "utils/Json.h"
+#include "dog_segment/ConcurrentVector.h"
+#include <random>
+#include <vector>
+#include <thread>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using namespace milvus::engine;
+using namespace milvus::dog_segment;
+using std::vector;
+
+TEST(ConcurrentVector, TestABI) {
+    ASSERT_EQ(TestABI(), 42);
+    assert(true);
+}
+
+TEST(ConcurrentVector, TestSingle) {
+    auto dim = 8;
+    ConcurrentVector<int, 32> c_vec(dim);
+    std::default_random_engine e(42);
+    int data = 0;
+    auto total_count = 0;
+    for(int i = 0; i < 10000; ++i) {
+        int insert_size = e() % 150;
+        vector<int> vec(insert_size * dim);
+        for(auto& x: vec) {
+            x = data++;
+        }
+        c_vec.grow_to_at_least(total_count + insert_size);
+        c_vec.set_data(total_count, vec.data(), insert_size);
+        total_count += insert_size;
+    }
+    ASSERT_EQ(c_vec.chunk_size(), (total_count + 31) / 32);
+    for(int i = 0; i < total_count; ++i) {
+        for(int d = 0; d < dim; ++d) {
+            auto std_data = d + i * dim;
+            ASSERT_EQ(c_vec.get_element(i)[d], std_data);
+        }
+    }
+}
+
+
+
+TEST(ConcurrentVector, TestMulti) {
+    auto dim = 8;
+    constexpr int threads = 16;
+    std::vector<int64_t> total_counts(threads);
+
+    ConcurrentVector<int64_t, 32> c_vec(dim);
+    std::atomic<int64_t> ack_counter = 0;
+
+    auto executor = [&](int thread_id) {
+        std::default_random_engine e(42);
+        int64_t data = 0;
+        auto total_count = 0;
+        for(int i = 0; i < 10000; ++i) {
+            int insert_size = e() % 150;
+            vector<int64_t> vec(insert_size * dim);
+            for(auto& x: vec) {
+                x = data++ * threads + thread_id;
+            }
+            auto offset = ack_counter.fetch_add(insert_size);
+            c_vec.grow_to_at_least(offset + insert_size);
+            c_vec.set_data(total_count, vec.data(), insert_size);
+            total_count += insert_size;
+        }
+        total_counts[thread_id] = total_count;
+    };
+    std::vector<std::thread> pool;
+    for(int i = 0; i < threads; ++i) {
+        pool.emplace_back(executor, i);
+    }
+    for(auto &thread: pool) {
+        thread.join();
+    }
+
+    std::vector<int64_t> counts(threads);
+    auto N = ack_counter.load();
+    for(int64_t i = 0; i < N; ++i) {
+        for(int d = 0; d < dim; ++d) {
+            auto data = c_vec.get_element(i)[d];
+            auto thread_id = data % threads;
+            auto raw_data =  data / threads;
+            auto std_data = counts[thread_id]++;
+            ASSERT_EQ(raw_data, std_data);
+        }
+    }
+
+}
